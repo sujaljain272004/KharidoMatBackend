f86b1fb8f7969e79b2086897371f1d32e7fc59f8
sujaljain272004 <160271734+sujaljain272004@users.noreply.github.com>
2025-07-13 12:36:45 +0530

    Swagger added

--- kharidoMat/src/main/java/com/SpringProject/kharidoMat/model/LoginRequest.java
+++ kharidoMat/src/main/java/com/SpringProject/kharidoMat/model/LoginRequest.java
@@ -1,23 +1,37 @@
 package com.SpringProject.kharidoMat.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Login request containing user's email and password")
 public class LoginRequest {
-	private String email;
-	private String password;
 
-	public String getEmail() {
-		return email;
-	}
+    @Schema(
+        description = "Email address registered with the application",
+        example = "sujal@college.edu",
+        required = true
+    )
+    private String email;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    @Schema(
+        description = "Password associated with the user's account",
+        example = "mySecret123",
+        required = true
+    )
+    private String password;
 
-	public String getPassword() {
-		return password;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
